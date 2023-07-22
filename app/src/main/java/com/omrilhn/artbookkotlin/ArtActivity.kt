75dd3c6b78b8dd9dca09a78c15c9bb8f348ea748
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-07-22 12:08:40 +0300

    ArtActivity.kt update and viewBinding added

--- app/src/main/java/com/omrilhn/artbookkotlin/ArtActivity.kt
+++ app/src/main/java/com/omrilhn/artbookkotlin/ArtActivity.kt
@@ -2,10 +2,25 @@ package com.omrilhn.artbookkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.omrilhn.artbookkotlin.databinding.ActivityArtBinding
 
 class ArtActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityArtBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_art)
+        binding = ActivityArtBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        fun saveButtonClicked(view:View)
+        {
+
+        }
+        fun selectImage(view:View)
+        {
+
+        }
     }
 }
